6ad0d5ce13786b31b8840601e0e9f5e3798eb1c4
Jose Dominguez <jos@tchpc.tcd.ie>
2011-02-23 16:23:18 +0000

    created step definitions for both acceptance scenarios around 1999

--- josmas/features/roman_numerals.feature
+++ josmas/features/roman_numerals.feature
@@ -5,5 +5,10 @@ Feature: Roman and arabic numeral equivalences
 
   Scenario: Convert from Roman to Arabic
     Given a number in arabic such as 1999
-    When I run the converter
+    When I run the arabic converter
     Then I should get the value MCMXCIX
+
+  Scenario: Convert from Arabic to Roman 
+    Given a number in roman such as "MCMXCIX"
+    When I run the roman converter
+    Then I should get the value 1999
--- josmas/src/test/java/com/jos/katas/romannumerals/stepdefinitions/RomanNumeralSteps.java
+++ josmas/src/test/java/com/jos/katas/romannumerals/stepdefinitions/RomanNumeralSteps.java
@@ -1,23 +1,45 @@
 package com.jos.katas.romannumerals.stepdefinitions;
 
+import com.jos.katas.romannumerals.RomanNumerals;
+
 import cuke4duke.annotation.I18n.EN.Given;
 import cuke4duke.annotation.I18n.EN.Then;
 import cuke4duke.annotation.I18n.EN.When;
-import cuke4duke.annotation.Pending;
+import static org.junit.Assert.*;
 
 public class RomanNumeralSteps {
+	private RomanNumerals rn = new RomanNumerals();
+	private int arabic;
+	private String roman = "";
+	
     @Given("a number in arabic such as (\\d+)")
-    @Pending
-    public void arabicToRoman(int arabic){
+    public void arabicToRoman(int arabic){;
+    	this.arabic = arabic;
     }
     
-    @When("^I run the converter$")
-    @Pending
-    public void iRunTheConverter() {
+    @When("^I run the arabic converter$")
+    public void iRunTheArabicConverter() {
+    	this.roman = rn.convert(this.arabic);
     }
     
     @Then("^I should get the value MCMXCIX$")
-    @Pending
     public void iShouldGetTheValueMCMXCIX() {
+    	assertTrue((this.roman).equals("MCMXCIX"));
     }
+        
+    @Given("^a number in roman such as \"([^\"]*)\"$")
+    public void aNumberInRomanSuchAsMCMXCIX(String roman) {
+    	this.roman = roman;
+    }
+    
+    @When("^I run the roman converter$")
+    public void iRunTheRomanConverter() {
+    	this.arabic = rn.convert(this.roman);
+    }
+     
+    @Then("^I should get the value 1999$")
+    public void iShouldGetTheValue1999() {
+    	assertTrue(this.arabic == 1999);
+    }
+
 }
