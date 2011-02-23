a8242a2a97135e936556775d53f73d5d0ef2b8a1
Jose Dominguez <jos@tchpc.tcd.ie>
2011-02-23 15:14:19 +0000

    Changed cuke4duke from Jruby execution to Maven

---
+++ josmas/src/test/java/com/jos/katas/romannumerals/stepdefinitions/RomanNumeralSteps.java
@@ -0,0 +1,23 @@
+package com.jos.katas.romannumerals.stepdefinitions;
+
+import cuke4duke.annotation.I18n.EN.Given;
+import cuke4duke.annotation.I18n.EN.Then;
+import cuke4duke.annotation.I18n.EN.When;
+import cuke4duke.annotation.Pending;
+
+public class RomanNumeralSteps {
+    @Given("a number in arabic such as (\\d+)")
+    @Pending
+    public void arabicToRoman(int arabic){
+    }
+    
+    @When("^I run the converter$")
+    @Pending
+    public void iRunTheConverter() {
+    }
+    
+    @Then("^I should get the value MCMXCIX$")
+    @Pending
+    public void iShouldGetTheValueMCMXCIX() {
+    }
+}
